782863a617f1cb0999d2473715652e35b082a8e0
ShuYing9156 <ruby2015095231128@gmai.com>
2025-07-15 21:49:32 +0800

    Compare the FASTA file from NCBI and PDB against UniProt.

--- py_file/04_cdhit2d.py
+++ py_file/04_cdhit2d.py
@@ -1,13 +1,13 @@
 from func import *
 
 exe = './cd-hit-v4.8.1-2019-0228/cd-hit-2d'
-input1 = './positive_data/uniprot/uniprot_pos_fil_cdhit40.fasta'
+input1 = './negative_data/uniprot/uniprot_neg_fil_cdhit40.fasta'
 
-input2 = ['./positive_data/ncbi/ncbi_pos_filtered.fasta',
-          './positive_data/pdb/pdb_pos_filtered.fasta']
+input2 = ['./negative_data/ncbi/ncbi_neg_filtered.fasta',
+          './negative_data/pdb/pdb_neg_filtered.fasta']
 
-output = ['./positive_data/ncbi/ncbi_uniprot_cdhit2d40.fasta',
-          './positive_data/pdb/pdb_uniprot_cdhit2d40.fasta']
+output = ['./negative_data/ncbi/ncbi_uniprot_cdhit2d40.fasta',
+          './negative_data/pdb/pdb_uniprot_cdhit2d40.fasta']
 
 identity = 0.4
 word_size = 2
